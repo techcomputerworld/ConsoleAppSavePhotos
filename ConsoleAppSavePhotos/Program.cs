83691ead00214182b0f64b857f133512e2353f22
Techcomputerworld <techcomputerworld@outlook.com>
2023-04-30 12:40:11 +0200

    allow the use of folders within the user

--- ConsoleAppSavePhotos/Program.cs
+++ ConsoleAppSavePhotos/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleAppSavePhotos;
 
-if (args.Length == 0)
+
+string carpeta = "";
+var os = Environment.OSVersion;
+if (args.Length <= 1)
 {
     Console.WriteLine("Hello, this program is copy startup photos, first version for Windows 10 / 11");
     Console.WriteLine("come on copy startup photos on Windows 10/11");
@@ -9,9 +12,13 @@ if (args.Length == 0)
         "Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets");
     Console.WriteLine("Press enter to proceed the copy startup photos if you don't want to copy, press another key");
     SavePhotos savePhotos = new SavePhotos();
+    carpeta = args[0].ToString();
     if (Console.ReadKey().Key == ConsoleKey.Enter)
     {
-        savePhotos.SavePhotoWindows();
+        savePhotos.SavePhotoWindows(carpeta);
+        Console.WriteLine("Current OS Information: \n");
+        Console.WriteLine("Platform: {0:G}", os.Platform);
+        Console.WriteLine("Platform: {0:G}", os.VersionString);
         Console.WriteLine("Ok thank you for using the software Save Photos Startup on Windows 10 / 11");
         Console.WriteLine("Created by Techcomputerworld");
         Console.WriteLine("https://techcomputerworld.com");
@@ -27,11 +34,11 @@ if (args.Length == 0)
         {
             
             //detectar en que sistema operativo estamos antes de realizar el proceso.
-            var os = Environment.OSVersion;
+            
             Console.WriteLine("Current OS Information: \n");
             Console.WriteLine("Platform: {0:G}", os.Platform);
             Console.WriteLine("Platform: {0:G}", os.VersionString);
-            savePhotos.SavePhotoWindows();
+            savePhotos.SavePhotoWindows(carpeta);
         }
         else if (read == "no")
         {
--- ConsoleAppSavePhotos/SavePhotos.cs
+++ ConsoleAppSavePhotos/SavePhotos.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,14 +14,42 @@ namespace ConsoleAppSavePhotos
         public string PathUser { get; set; }
         public string FileStartupPhoto { get; set; }
         public string FileUserPhoto { get; set; }
-
-        public void SavePhotoWindows()
+        //string carpeta va a ser para decir a que carpeta vamos a copiar todas las fotos
+        //este metodo para uan aplicación de consola
+        public void SavePhotoWindows(string carpeta)
         {
             string userName = Environment.UserName;
             PathStartupPhotos = Path.GetFullPath("C:\\Users\\" + userName + "\\AppData\\Local\\Packages"
                 + "\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\" +
                  "LocalState\\Assets");
             PathUser = Path.GetFullPath("C:\\Users\\" + userName);
+            if (carpeta == "documents")
+            {
+                PathUser = Path.GetFullPath("C:\\Users\\" + userName + "\\Documents");
+            }
+            else if (carpeta == "Downloads")
+            {
+                PathUser = Path.GetFullPath("C:\\Users\\" + userName + "\\Downloads");
+            }
+            else
+            {
+                PathUser = Path.GetFullPath("C:\\Users\\" + userName + "\\" + carpeta);
+
+            }
+            //comprobar que existe el directorio
+            try
+            {
+                DirectoryInfo directory = new DirectoryInfo(PathUser);
+                if (!directory.Exists)
+                {
+                    Directory.CreateDirectory(PathUser);
+                }
+               
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
 
             try
             {
@@ -29,16 +58,17 @@ namespace ConsoleAppSavePhotos
                 {
                     FileStartupPhoto = Path.GetFileName(file.ToString());
                     FileUserPhoto = Path.GetFileName(FileStartupPhoto + ".jpg");
-                    
+                    //
                     //quiero cambiar el nombre del fichero pero no sé como hacerlo porque puedo darle a elegir al usuario
                     File.Copy(Path.Combine(PathStartupPhotos, FileStartupPhoto), Path.Combine(PathUser, FileUserPhoto), true);
                 }
                 Console.WriteLine("images copied successfully");
                 
             } 
-            catch (Exception ex)
+            catch (Exception e)
             {
-                Console.WriteLine(ex.Message);
+                //Console.WriteLine(ex.Message);
+                string exception = e.ToString();
             }
         }
     }
