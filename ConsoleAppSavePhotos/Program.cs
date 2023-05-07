362a9f9530ed5ae81bc1b4bb3350e8b3d2f4ba14
Techcomputerworld <techcomputerworld@outlook.com>
2023-05-07 14:01:44 +0200

    all problem solved for copy your startup photos

--- ConsoleAppSavePhotos/Program.cs
+++ ConsoleAppSavePhotos/Program.cs
@@ -2,25 +2,66 @@
 using ConsoleAppSavePhotos;
 
 
-string carpeta = "";
-var os = Environment.OSVersion;
-Hello hello = new Hello();
-SavePhotos savePhotos = new SavePhotos();
-if (args.Length == 0)
+class program
 {
-    hello.Welcome();
-    if (Console.ReadKey().Key == ConsoleKey.Enter)
+    
+    static void Main(string[] args)
     {
-        savePhotos.SavePhotoWindows();
-        Console.WriteLine("Current OS Information: \n");
-        Console.WriteLine("Platform: {0:G}", os.Platform);
-        Console.WriteLine("Platform: {0:G}", os.VersionString);
-        Console.WriteLine("Ok thank you for using the software");
-        Console.WriteLine("Created by Techcomputerworld");
-        Console.WriteLine("https://techcomputerworld.com");
-    }
-    else
+        string carpeta = "";
+        var os = Environment.OSVersion;
+        Hello hello = new Hello();
+        SavePhotos savePhotos = new SavePhotos();
+        if (args.Length == 0)
+        {
+            hello.Welcome();
+            if (Console.ReadKey().Key == ConsoleKey.Enter)
+            {
+                savePhotos.SavePhotoWindows();
+                Console.WriteLine("Current OS Information: \n");
+                Console.WriteLine("Platform: {0:G}", os.Platform);
+                Console.WriteLine("Platform: {0:G}", os.VersionString);
+                Console.WriteLine("Ok thank you for using the software");
+                Console.WriteLine("Created by Techcomputerworld");
+                Console.WriteLine("https://techcomputerworld.com");
+            }
+            else
+            {
+                OtherKey();
+            }
+
+        }
+
+        if (args.Length == 1)
+        {
+
+
+            hello.Welcome();
+            carpeta = args[0].ToString();
+            if (Console.ReadKey().Key == ConsoleKey.Enter)
+            {
+                savePhotos.SavePhotoWindows(carpeta);
+                Console.WriteLine("Current OS Information: \n");
+                Console.WriteLine("Platform: {0:G}", os.Platform);
+                Console.WriteLine("Platform: {0:G}", os.VersionString);
+                Console.WriteLine("Ok thank you for using the software Save Photos Startup on Windows 10 / 11");
+                Console.WriteLine("Created by Techcomputerworld");
+                Console.WriteLine("https://techcomputerworld.com");
+                
+            }
+            else
+            {
+                OtherKey();
+            }
+
+        }
+
+    } //final metodo main
+    static void OtherKey()
     {
+        SavePhotos savePhotos = new SavePhotos();
+        var os = Environment.OSVersion;
+        string carpeta = "";
+
         Console.WriteLine("Are you sure that don't copy your startup photos?");
         Console.WriteLine("could you response: yes or no");
         Console.WriteLine("if press enter for default response is enter.");
@@ -33,9 +74,6 @@ if (args.Length == 0)
             Console.WriteLine("Current OS Information: \n");
             Console.WriteLine("Platform: {0:G}", os.Platform);
             Console.WriteLine("Platform: {0:G}", os.VersionString);
-            Console.WriteLine("Ok thank you for using the software");
-            Console.WriteLine("Created by Techcomputerworld");
-            Console.WriteLine("https://techcomputerworld.com");
             savePhotos.SavePhotoWindows(carpeta);
         }
         else if (read == "no")
@@ -46,51 +84,6 @@ if (args.Length == 0)
 
         }
     }
-
-}
-
-if (args.Length == 1)
-{
-    
-    
-    hello.Welcome();
-    carpeta = args[0].ToString();
-    if (Console.ReadKey().Key == ConsoleKey.Enter)
-    {
-        savePhotos.SavePhotoWindows(carpeta);
-        Console.WriteLine("Current OS Information: \n");
-        Console.WriteLine("Platform: {0:G}", os.Platform);
-        Console.WriteLine("Platform: {0:G}", os.VersionString);
-        Console.WriteLine("Ok thank you for using the software Save Photos Startup on Windows 10 / 11");
-        Console.WriteLine("Created by Techcomputerworld");
-        Console.WriteLine("https://techcomputerworld.com");
-        Console.Read();
-    }
-    else
-    {
-        Console.WriteLine("Are you sure that don't copy your startup photos?");
-        Console.WriteLine("could you response: yes or no");
-        Console.WriteLine("if press enter for default response is enter.");
-        String read = Console.ReadLine();
-        if (read == "yes" || read == "")
-        {
-            
-            //detectar en que sistema operativo estamos antes de realizar el proceso.
-            
-            Console.WriteLine("Current OS Information: \n");
-            Console.WriteLine("Platform: {0:G}", os.Platform);
-            Console.WriteLine("Platform: {0:G}", os.VersionString);
-            savePhotos.SavePhotoWindows(carpeta);
-        }
-        else if (read == "no")
-        {
-            Console.WriteLine("Ok thank you for using the software");
-            Console.WriteLine("Created by Techcomputerworld");
-            Console.WriteLine("https://techcomputerworld.com");
-
-        }
-    }
-    
 }
 
 
